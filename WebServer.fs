032d263ea5e71c2692788afcf7fd8aa5b464251e
pawelwicher <34919112+pawelwicher@users.noreply.github.com>
2017-12-28 20:08:44 +0100

    Add files via upload

---
+++ WebServer.fs
@@ -0,0 +1,20 @@
+﻿module WebServer
+
+open System
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Functions
+
+let app =
+    choose
+        [ GET >=> choose
+            [ 
+                path "/hello" >=> OK hello
+                pathScan "/fib/%d" (fun n -> OK ((fib n).ToString()) )
+            ]
+        ]
+
+let start = startWebServer defaultConfig app
+
