ba6c3222f788929f8f64fae035b7b7e511e8bf17
pawelwicher <34919112+pawelwicher@users.noreply.github.com>
2018-01-02 11:16:53 +0100

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+
+let hello = "Hello World !!!"
+
+let square x =
+    let p = x in p * p
+
+let rec fib n =
+    match n with
+    | x when x < 1 -> failwith("invalid arg")
+    | 1 -> 1
+    | 2 -> 1
+    | _ -> fib(n-1) + fib(n-2)
+
+let app =
+    choose
+        [ GET >=> choose
+            [
+                path "/" >=> OK hello
+                pathScan "/square/%d" (fun n -> OK ((square n).ToString()) )
+                pathScan "/fib/%d" (fun n -> OK ((fib n).ToString()) )
+            ]
+        ]
+
+[<EntryPoint>]
+let main argv =
+    startWebServer defaultConfig app
+    0
