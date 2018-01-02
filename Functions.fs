33e58190c7ab34943c41051debf1f7843c5a605c
pawelwicher <34919112+pawelwicher@users.noreply.github.com>
2018-01-02 10:32:57 +0100

    Add files via upload

---
+++ Functions.fs
@@ -0,0 +1,16 @@
+﻿module Functions
+
+let hello = "Hello World !!!"
+
+let inline add a b = a + b
+
+let rec fib n =
+    match n with
+    | x when x < 1 -> failwith("invalid arg")
+    | 1 -> 1
+    | 2 -> 1
+    | _ -> fib(n-1) + fib(n-2)
+
+
+let square x =
+    let p = x in p * p
